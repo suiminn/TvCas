1d2bdee29bdc7bd5a4b18d622a895b4bfce5d418
tsukumijima <tsukumijima@users.noreply.github.com>
2021-02-16 00:21:46 +0900

    SPHD.tvcas ビルド機能を削除 SPHD.tvcas は実際にはメタデータを書き換えただけの B25.tvcas で全く機能しないらしい（ハッシュこそ異なるもののファイルサイズも同一）

--- TVCAS_B25/TVCAS_B25.h
+++ TVCAS_B25/TVCAS_B25.h
@@ -11,17 +11,10 @@
 	ReadMe.txt Ç‡ì«ÇÒÇ≈Ç≠ÇæÇ≥Ç¢ÅB
 */
 
-#ifdef SPHD
-#define TVCAS_MODULE_NAME				"SPHD"
-#define TVCAS_MODULE_COPYRIGHT			"2sen + Logue"
-#define TVCAS_MODULE_ORIGINAL_FILE_NAME	"SPHD.tvcas"
-#define TVCAS_MODULE_VERSION			"0.9.1"
-#else
 #define TVCAS_MODULE_NAME				"B25"
 #define TVCAS_MODULE_COPYRIGHT			"2sen + Logue"
 #define TVCAS_MODULE_ORIGINAL_FILE_NAME	"B25.tvcas"
 #define TVCAS_MODULE_VERSION			"0.9.1"
-#endif
 
 #define TVCAS_MODULE_VERSION_MAJOR	0
 #define TVCAS_MODULE_VERSION_MINOR	9
