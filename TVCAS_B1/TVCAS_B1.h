8c2b9e57d70c22b474c1a3228f12d0fcaaf3a78b
tsukumijima <tsukumijima@users.noreply.github.com>
2021-02-16 21:16:14 +0900

    B1.tvcas（ SPHD 用の tvcas ファイル）のビルド機能を追加 TVCAS_B1 は TVCAS_B25 のコードに TVCAS_B25_SPHD.diff.txt を適用し、ファイル名の変更やプロジェクトの設定調整などを行って SPHD に対応させたもの 出力ファイル名は B1Decoder などとの統一の観点から SPHD.tvcas から B1.tvcas に変更した

---
+++ TVCAS_B1/TVCAS_B1.h
@@ -0,0 +1,24 @@
+#ifndef TVCAS_B1_H
+#define TVCAS_B1_H
+
+
+/*
+	改変する場合、以下の条件に従ってください。
+
+	・ファイル名をオリジナルと異なるものにすること。
+	・著作権表記を書き換えること。
+
+	ReadMe.txt も読んでください。
+*/
+
+#define TVCAS_MODULE_NAME				"B1"
+#define TVCAS_MODULE_COPYRIGHT			"2sen + Logue"
+#define TVCAS_MODULE_ORIGINAL_FILE_NAME	"B1.tvcas"
+#define TVCAS_MODULE_VERSION			"0.9.1"
+
+#define TVCAS_MODULE_VERSION_MAJOR	0
+#define TVCAS_MODULE_VERSION_MINOR	9
+#define TVCAS_MODULE_VERSION_BUILD	1
+
+
+#endif
